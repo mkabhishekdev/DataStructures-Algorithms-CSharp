c636fc6554df7c4bc205150a13e9a878380f2e99
M.K.Abhishek <mkabhishek@192.168.1.9>
2025-06-16 21:20:16 +0530

    ds: stack - using ll

--- DataStructures/DS3_1_StackUsingArrayImpl.cs
+++ DataStructures/DS3_1_StackUsingArrayImpl.cs
@@ -48,3 +48,16 @@ public class DS3_1_StackUsingArrayImpl
         }
     }
 }
+
+/*
+    DS3_1_StackUsingArrayImpl ds3_1 = new DS3_1_StackUsingArrayImpl();
+
+        ds3_1.Push(1);
+        ds3_1.Push(2);
+        ds3_1.Push(3);
+        ds3_1.Push(4);
+        ds3_1.Push(5);
+        ds3_1.Pop();
+        ds3_1.DisplayStack();
+        Console.WriteLine("The peek item is: " +ds3_1.Peek());
+*/
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -4,16 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        DS3_1_StackUsingArrayImpl ds3_1 = new DS3_1_StackUsingArrayImpl();
-
-        ds3_1.Push(1);
-        ds3_1.Push(2);
-        ds3_1.Push(3);
-        ds3_1.Push(4);
-        ds3_1.Push(5);
-        ds3_1.Pop();
-        ds3_1.DisplayStack();
-        Console.WriteLine("The peek item is: " +ds3_1.Peek());
-        
+        DS3_2_StackUsingLinkedListImpl ds32 = new DS3_2_StackUsingLinkedListImpl();
+        ds32.Push(2);
+        ds32.Push(5);
+        ds32.Push(8);
+        ds32.Display();
+        ds32.Peek();
+        ds32.Pop();
+        ds32.Peek();
     }
 }
