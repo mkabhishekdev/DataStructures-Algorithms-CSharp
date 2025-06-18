6e230b2e80843b111a6541c71622f5bbc3134759
M.K.Abhishek <mkabhishek@192.168.1.6>
2025-06-18 22:12:33 +0530

    ds: queue - using array

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -4,13 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        DS3_2_StackUsingLinkedListImpl ds32 = new DS3_2_StackUsingLinkedListImpl();
-        ds32.Push(2);
-        ds32.Push(5);
-        ds32.Push(8);
-        ds32.Display();
-        ds32.Peek();
-        ds32.Pop();
-        ds32.Peek();
+    
     }
 }
--- DataStructures/obj/Debug/net8.0/DataStructures.AssemblyInfo.cs
+++ DataStructures/obj/Debug/net8.0/DataStructures.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataStructures")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db988cc53b23ce232842b7f2bee037031aee0b2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c636fc6554df7c4bc205150a13e9a878380f2e99")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataStructures")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataStructures")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
