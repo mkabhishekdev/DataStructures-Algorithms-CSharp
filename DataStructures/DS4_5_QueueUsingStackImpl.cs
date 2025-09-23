0766c889d5f7360bad52b4dc79edaca0cdf692ad
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2025-09-23 20:20:16 +0530

    need to finish binary tree impl tomorrow

--- DataStructures/DS4_5_QueueUsingStackImpl.cs
+++ DataStructures/DS4_5_QueueUsingStackImpl.cs
@@ -88,3 +88,15 @@ public class DS4_5_QueueUsingStackImpl
     }
 
 }  
+
+
+/*
+  DS4_5_QueueUsingStackImpl ds45 = new DS4_5_QueueUsingStackImpl();
+        ds45.Enqueue(4);
+        ds45.Enqueue(11);
+        ds45.Enqueue(7);
+        ds45.Enqueue(2);
+        ds45.Enqueue(6);
+        ds45.Dequeue();
+        ds45.Display();
+*/
