de312e5db4b822cff7c4dabe68746a0abf78e860
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2025-10-02 13:12:36 +0530

    code practice folder updated

--- ProductFaangCodingPractice/StackQueue/PracticeOne/ProblemTwoImplementStackUsingQueue.cs
+++ ProductFaangCodingPractice/StackQueue/PracticeOne/ProblemTwoImplementStackUsingQueue.cs
@@ -3,45 +3,70 @@ using System.Collections;
 
 public class ProblemTwoImplementStackUsingQueue()
 {
+    //push(data),pop(),peek(),display()
 
-    Queue<int> queue = new Queue<int>();
-    List<int> store = new List<int>();
+    Queue<int> primaryQueue = new Queue<int>();
+    Queue<int> helperQueue = new Queue<int>();
 
-    int queueSize = 0;
-
-    public void Push(int data)
+    public void Push(int inputData)
     {
-        queue.Enqueue(data);
-        queueSize++;
-        Console.WriteLine("Element inserted into the stack");
+        if (primaryQueue.Count == 0)
+        {
+            primaryQueue.Enqueue(inputData);
+            return;
+        }
+
+        while (primaryQueue.Count != 0)
+        {
+            helperQueue.Enqueue(primaryQueue.Peek());
+            primaryQueue.Dequeue();
+        }
+        primaryQueue.Enqueue(inputData);
+        while (helperQueue.Count != 0)
+        {
+            primaryQueue.Enqueue(helperQueue.Peek());
+            helperQueue.Dequeue();
+        }
+
+        Console.WriteLine("Element added to the stack successfully");
     }
+
 
     public void Pop()
     {
-        foreach (var item in queue)
-        {
-            store.Add(item);
-        }
-        
+        primaryQueue.Dequeue();
+        Console.WriteLine("Pop operation finished");
     }
 
-    public void Peek()
+
+    public int Peek()
     {
-        foreach (var item in queue)
-        {
-            store.Add(item);
-        }
-        Console.WriteLine("Element in the top of the stack is: " + store.ElementAt(queueSize));
+        int top = primaryQueue.Peek();
+        return top;
     }
+
 
     public void Display()
     {
         Console.WriteLine("Elements in the stack are: ");
-        
-        for (int i = store.Count; i > 0; i--)
+        foreach (var item in primaryQueue)
         {
-            Console.WriteLine(store.ElementAt(i));
+            Console.WriteLine(item);
         }
     }
 
+
 }
+
+/*
+     ProblemTwoImplementStackUsingQueue suq = new ProblemTwoImplementStackUsingQueue();
+        suq.Push(1);
+        suq.Push(2);
+        suq.Push(3);
+        suq.Push(4);
+        suq.Display();
+        Console.WriteLine("Element in top of the stack is: " + suq.Peek());
+        suq.Pop();
+        suq.Display();
+        Console.WriteLine("Element in top of the stack is: " + suq.Peek());
+*/
