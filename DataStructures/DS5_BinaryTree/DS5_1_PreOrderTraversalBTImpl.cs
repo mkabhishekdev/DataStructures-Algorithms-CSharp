1267b677240bfb6605fd97ab206e9a23477a8bbf
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2025-10-02 19:38:19 +0530

    folders restructured for clarity & binary trees started

---
+++ DataStructures/DS5_BinaryTree/DS5_1_PreOrderTraversalBTImpl.cs
@@ -0,0 +1,25 @@
+//PreOrder traversal (Type = DFS) [root -> left -> right]
+
+public class DS5_1_PreOrderTraversalBTImpl
+{
+    public class TreeNode
+    {
+        public TreeNode left;
+        public TreeNode right;
+        public int data;
+
+        public TreeNode(int x)
+        {
+            data = x;
+            left = null;
+            right = null;
+        }
+    }
+
+    public void PreOrderTraversal()
+    {
+        
+    }
+
+
+}
