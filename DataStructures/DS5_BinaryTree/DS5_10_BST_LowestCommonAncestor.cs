a5233e7b03996b5090cc09a37ea200c805eac563
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2025-10-21 19:14:06 +0530

    DS: trees - incomplete lower common ancestor problem

---
+++ DataStructures/DS5_BinaryTree/DS5_10_BST_LowestCommonAncestor.cs
@@ -0,0 +1,17 @@
+using System;
+using DataStructures.BinaryTree;
+
+public class DS5_10_BST_LowestCommonAncestor
+{
+    public int LowestCommonAncestor(TreeNode root, TreeNode firstNode, TreeNode secondNode)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+        
+     
+        
+    }
+    
+}
