77b53da02484f2de7f8a122f59bd938921395b36
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2025-10-25 16:39:09 +0530

    weekend coding test: 58%

---
+++ ProductFaangCodingPractice/BinaryTrees/SET1/InOrder.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductFaangCodingPractice.BinaryTrees;
+
+
+namespace ProductFaangCodingPractice.BinaryTrees.SET1
+{
+    public class InOrder
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            IList<int> result = null;
+
+            if (root == null)
+            {
+                return null;
+            }
+
+            InorderTraversal(root.left);
+            result.Add(root.data);
+            InorderTraversal(root.right);
+
+            return result;
+        }
+
+    }
+}
+
+
+// SCORE = 3/10
+
+/*
+  IList<int> result = new IList<int>();
+            Stack<int> myStack = new Stack<int>();
+
+            while (root.left != null)
+            {
+                root = root.left;
+            }
+            myStack.Push(root);
+
+            while(myStack.Count > 0)
+            {
+                root = myStack.Pop();
+                result.Add(root.data);
+
+                
+            }
+*/
