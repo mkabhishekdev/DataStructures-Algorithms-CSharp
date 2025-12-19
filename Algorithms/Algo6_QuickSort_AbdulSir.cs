65b2ef28a9ea2f5b8ace181de5535565195c1113
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2025-12-19 20:40:44 +0530

    algorithm work

--- Algorithms/Algo6_QuickSort_AbdulSir.cs
+++ Algorithms/Algo6_QuickSort_AbdulSir.cs
@@ -7,6 +7,55 @@ namespace Algorithms
 {
     public class Algo6_QuickSort_AbdulSir
     {
+        public int[] QuickSort(int[] input)
+        {
+            QuickSortImpl(input, 0, input.Length - 1);
+            return input;
+        }
+        
+        private void QuickSortImpl(int[] input, int low, int high)
+        {
+            if (low < high)
+            {
+                int j = Partition(input,low, high);
+                QuickSortImpl(input, low, j);
+                QuickSortImpl(input, j + 1, high);
+            }
+        }
+
+        public int Partition(int[] input,int low, int high)
+        {
+            int pivot = input[low];
+            int i = low;
+            int j = high+1;
+
+            while (i < j)
+            {
+                do
+                {
+                    i++;
+                } while (i <= high && input[i] <= pivot);
+
+                do
+                {
+                    j--;
+                } while (input[j] > pivot);
+
+                if (i < j)
+                {
+                    Swap(input, i, j);
+                }
+            }
+            Swap(input, low, j);
+            return j;
+        }
+
+        public void Swap(int[] input,int x, int y)
+        {
+            int temp = input[x];
+            input[x] = input[y];
+            input[y] = temp;
+        }
         
     }
 }
