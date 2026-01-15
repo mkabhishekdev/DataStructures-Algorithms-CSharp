5bfcdd0f7b0950f5569a52e86ccfcc1c9b3ea696
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2026-01-15 16:24:34 +0530

    problem solved: own approach solution

--- ProductCodingPractice/Trees/YourTHINKINGWork/SameTree.cs
+++ ProductCodingPractice/Trees/YourTHINKINGWork/SameTree.cs
@@ -7,8 +7,11 @@ using ProductCodingPractice.BinaryTrees;
 using System.Linq;
 
 /*
-MY APPROACH:
-
+APPROACH I CAME UP WITH:
+1. Use a Queue 
+2. First push the corresponding elements from both p and q in the form Queue<(TreeNode,TreeNode)> into the queue
+3. Check if condition for all failing conditions
+4. If not, Enqueue the next left,right sub tree nodes
 */
 
 namespace ProductCodingPractice.Trees.YourTHINKINGWork
@@ -29,8 +32,34 @@ namespace ProductCodingPractice.Trees.YourTHINKINGWork
             {
                 return false;
             }
+      
+            
+            Queue<(TreeNode, TreeNode)> myQueue = new Queue<(TreeNode, TreeNode)>();
 
-           
+            myQueue.Enqueue((p, q));
+
+            while (myQueue.Count > 0)
+            {
+                var (nodeP, nodeQ) = myQueue.Dequeue();
+
+                if (nodeP == null && nodeQ == null)
+                {
+                    continue;
+                }
+                if (nodeP == null || nodeQ == null)
+                {
+                    return false;
+                }
+                if(nodeP.val != nodeQ.val)
+                {
+                    return false;
+                }
+
+                myQueue.Enqueue((nodeP.left, nodeQ.left));
+                myQueue.Enqueue((nodeP.right, nodeQ.right));
+                
+            }
+          return true;                
         }
 
      
