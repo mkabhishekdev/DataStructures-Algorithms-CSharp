d3373c66e53ee21aa065841291822b050d9f114f
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2026-02-06 10:12:18 +0530

    daily problem solving

--- Algorithms/obj/Debug/net8.0/Algorithms.AssemblyInfo.cs
+++ Algorithms/obj/Debug/net8.0/Algorithms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+593dd6f7a7a80b3b4c3bf986f5932e65481aab0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aac3552c4a0b4e8cefff36c4267a199764b4f67a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProblemSolvingFromFirstPrinciples/Arrays/YourTHINKINGWork/MaximumSubArray.cs
+++ ProblemSolvingFromFirstPrinciples/Arrays/YourTHINKINGWork/MaximumSubArray.cs
@@ -6,29 +6,91 @@ using System.Threading.Tasks;
 namespace ProblemSolvingFromFirstPrinciples.Arrays.YourTHINKINGWork
 {
    /*
-   REFERRED APPROACH/APPROACH I CAME UP WITH:
-
-
-
+   REFERRED APPROACH
+   1. This is the approach called 'Kadanes Algorithm'
+   2. You start with the firstelement assigning it the currentSum and maxSum
+   3. Idea is to start from the index=1 and keep updating currentSum and maxSum
+   4. currentSum will be the max value between the present nums[index] or the currentSum+nums[index].
+      You need to store the max at each point
+   5. maxSum stores the max value between either currentSum or maxSum, maxSum keeps getting updated
+      on every iteration in the loop
+   6. return the maxSum
    */
-
+    
     /*
-    Time Complexity:
-    Space Complexity:
+    Time Complexity:O(n)
+    Space Complexity: O(1)
     */
 
     public class MaximumSubArray
     {
         public int MaxSubArray(int[] nums)
         {
+            if(nums.Length == 0)
+            {
+                return 0;
+            }
+            if(nums.Length == 1)
+            {
+                return 1;
+            }
+           
+            int currentSum = nums[0];
+            int maxSum = nums[0];
 
+            for(int i = 1; i < nums.Length; i++)
+            {
+                currentSum = Math.Max(nums[i], currentSum+nums[i]);
+                maxSum = Math.Max(currentSum, maxSum);
+            }
+            
+            return maxSum;
         }
 
     }
 }
 
     // If all test cases not clearing or You did not arrive at the solution on own, PRACTICE DEBUGGING MUST
-    /* FAILED APPROACH & WHY?
+    /* FAILED APPROACH & WHY?(Dictionary not needed, complicated the logic)
+    APPROACH I CAME UP WITH:
+   1. Store the total array sum in a variable say 'total'
+   2. Create a max variable and assign that value of total, now you have total array sum in max
+   3. Using loop, iterate through the array adding adjacent elements and comparing if the added value
+      is greater than max
+   4. If it is greater than max,have a list to save those elements and also have a list to save that sum value as
+      well. Here I want to do something where, I can have something like a key,value where the sum maps to
+      all the adjacent elements involved.(thinking how to do this?)
+   5. Than, I can pick the mapping of the max elements and return that has the max sub array
+       // CODE
+          public int MaxSubArray(int[] nums)
+        {
+            if(nums.Length == 0)
+            {
+                return 0;
+            }
+            if(nums.Length == 1)
+            {
+                return 1;
+            }
+            int total=0, max = 0;
+            Dictionary<(int,int),int> maxStorage = new Dictionary<(int, int), int>();
 
-       
+            for(int i = 0; i < nums.Length; i++)
+            {
+                total += nums[i];
+            }
+
+            max = total;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if((nums[i]+nums[i+1]) > max)
+                {
+                    max = nums[i] + nums[i+1];
+                    maxStorage.Add((nums[i],nums[i+1]),max); //something on this lines    
+                }
+            }
+            
+            return max;
+        }
     */
