fd59309dcb51ad715702a5e50404c9dd61402d81
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2026-02-10 13:35:14 +0530

    learning git rebase

--- TestYourSkill/obj/Debug/net8.0/TestYourSkill.AssemblyInfo.cs
+++ TestYourSkill/obj/Debug/net8.0/TestYourSkill.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestYourSkill")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3373c66e53ee21aa065841291822b050d9f114f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef4ebe263631246963a43956f3b764223f76575b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestYourSkill")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestYourSkill")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
