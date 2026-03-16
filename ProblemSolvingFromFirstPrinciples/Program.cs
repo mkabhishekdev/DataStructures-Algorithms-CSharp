2726a091b2a78164977132e030b22725c417b2d0
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2026-03-16 23:28:14 +0530

    need to debug the problem still

--- ProblemSolvingFromFirstPrinciples/Program.cs
+++ ProblemSolvingFromFirstPrinciples/Program.cs
@@ -2,14 +2,7 @@
 {
     static void Main(string[] args)
     {
-        int[][] input =
-        {
-            new int[] {0,1,2,0},
-            new int[] {3,4,5,2},
-            new int[] {1,3,1,5}  
-        };
-
-        SetZeroes sz = new SetZeroes();
-        sz.SetZeroesImpl(input);
+        MinimumWindow mw = new MinimumWindow();
+        mw.MinimumWindowSubstring("aebdecbcba","abcc");
     }
 }
