699b2836b5f4bfcf944cea71ee00422109b0f0ba
M.K.Abhishek <mkabhishek@MKAbhisheks-MacBook-Pro.local>
2026-03-30 22:51:05 +0530

    problem solving: trees -> palindromic substrings, subtree

--- ProblemSolvingFromFirstPrinciples/Strings/YourTHINKINGWork/PalindromicSubstrings.cs
+++ ProblemSolvingFromFirstPrinciples/Strings/YourTHINKINGWork/PalindromicSubstrings.cs
@@ -6,25 +6,44 @@ using System.Threading.Tasks;
 namespace ProblemSolvingFromFirstPrinciples.Strings.YourTHINKINGWork
 {
     /* My Approach
-    1. to start with the minimum number of palindromic substrings possible are the length of the string,
-       since each character in the string by itself is a palindrome
-    2. 
-    */
-
-    /* any optimization later
-
+      - logic to count from the centre
     */
 
     /*
-    time complexity:
-    space complexity:
+    time complexity: O(n2)
+    space complexity: O(1)
     */
 
     public class PalindromicSubstrings
     {
         public int CountSubstrings(string s)
         {
-            
+            int count = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                // Odd length palindromes (center at i)
+                count += CountPalindromes(s, i, i);
+
+                // Even length palindromes (center between i and i+1)
+                count += CountPalindromes(s, i, i + 1);
+            }
+
+            return count;
+        }
+
+        private int CountPalindromes(string s, int left, int right)
+        {
+            int count = 0;
+
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                count++;
+                left--;
+                right++;
+            }
+
+            return count;
         }
     }
 }
